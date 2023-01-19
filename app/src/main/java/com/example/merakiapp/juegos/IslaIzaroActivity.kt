e8b497b08ce6ed19fc660679dc9db496c44fb46e
endika villafruela <evillafruela2001@gmail.com>
2023-01-19 12:58:07 +0100

    socketIO

--- app/src/main/java/com/example/merakiapp/juegos/IslaIzaroActivity.kt
+++ app/src/main/java/com/example/merakiapp/juegos/IslaIzaroActivity.kt
@@ -4,13 +4,19 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.net.toUri
 import com.example.merakiapp.Dialogos
 import com.example.merakiapp.Explicaciones
 import com.example.merakiapp.databinding.ActivityIslaIzaroBinding
+import io.socket.client.IO
+import org.json.JSONArray
+import org.json.JSONObject
 
 class IslaIzaroActivity : AppCompatActivity(), Dialogos, Explicaciones {
     private lateinit var binding: ActivityIslaIzaroBinding
+    private lateinit var sala:String
+    var socket = IO.socket("https://merakiapp-servicio-multijugador.glitch.me")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Deshabilitar rotación de pantalla (Landscape)
@@ -24,13 +30,33 @@ class IslaIzaroActivity : AppCompatActivity(), Dialogos, Explicaciones {
 
         val id = intent.getIntExtra("id",0)
         val name = intent.getStringExtra("name")
-        val pasos = intent.getIntExtra("pasos",0)
         val imagen = intent.getStringExtra("imagen").toString()
+        socket.connect()
+        socket.emit("sala" , sala.toInt(),name,binding.barraUsuario?.progress ,imagen)
+
+
+
+
         binding.constraintLayout3.visibility = View.GONE
         binding.barraOponente?.isEnabled  =false
         binding.barraUsuario?.isEnabled  =false
         binding.txtUsuario.text= name
         binding.imagenUsuario.setImageURI(imagen.toUri())
+        socket.on("actualizar"){args->
+                var data = args[0] as JSONArray
+                for(pos in 0 until 2) {
+                    val valor = data[pos] as JSONObject
+                    if(valor["codigo"]==socket.id()){
+                        binding.txtUsuario.text = ""+valor["name"]
+                        binding.barraUsuario?.setProgress(valor["punt"] as Int)
+                    }else{
+                        binding.txtOponente.text = ""+valor["name"]
+                        binding.barraUsuario?.setProgress(valor["punt"] as Int)
+                        //binding.imagenOponente.setImageURI()
+
+                    }
+                }
+        }
 
         binding.button.setOnClickListener(){
             binding.constraintLayout3.visibility = View.VISIBLE
@@ -46,18 +72,10 @@ class IslaIzaroActivity : AppCompatActivity(), Dialogos, Explicaciones {
         binding.barraOponente?.setProgress(0)
 
         binding.botonMoverBarco?.setOnClickListener(){
-            val progreso = binding.barraUsuario?.getProgress()
-
-            if (progreso != null) {
-                binding.barraUsuario?.setProgress(progreso+1)
-            }
+            socket.emit("incrementation", name,binding.barraUsuario?.progress,imagen)
         }
         binding.btnSprint?.setOnClickListener(){
-            val progreso = binding.barraUsuario?.getProgress()
-
-            if (progreso != null) {
-                binding.barraUsuario?.setProgress(progreso+2)
-            }
+            socket.emit("incrementation2", name,binding.barraUsuario?.progress,imagen)
         }
         if(this.getSharedPreferences("pref", 0)?.getBoolean("libre", false) == false){
            // binding.btnVolverGaztelugatxe.visibility = View.VISIBLE
